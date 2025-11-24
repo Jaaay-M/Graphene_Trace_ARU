5bf895867c010c0cb77de79e46294246bfd4519e
Jaaay-M <jaymanning123@gmail.com>
2025-11-24 02:31:11 +0000

    Updated the patient model to match the EERD

--- Graphene Trace/Models/Patient.cs
+++ Graphene Trace/Models/Patient.cs
@@ -2,8 +2,16 @@
 {
     public class Patient
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public int PatientId { get; set; }
+        public string doctor_id { get; set; }
+        public string carer_id { get; set; }
+        public enum title { Mr, Mrs, Miss, Ms, Dr, Prof }
+        public string first_name { get; set; } 
+        public string last_name { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+        public string address { get; set; }
+
     }
 }
